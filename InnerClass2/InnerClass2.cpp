ace6cd194d474a3987c2bd97dd2a6d12f1d93df4
4elovel <marcynakvova007@gmail.com>
2022-09-17 15:29:06 +0300

    Добавьте файлы проекта.

---
+++ InnerClass2/InnerClass2.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Vector3D
+{
+private:
+	double m_x, m_y, m_z;
+
+public:
+	Vector3D(double x = 0.0, double y = 0.0, double z = 0.0)
+		: m_x(x), m_y(y), m_z(z)
+	{
+
+	}
+
+
+	class Point3D
+	{
+	private:
+		double m_x, m_y, m_z;
+
+	public:
+		Point3D(double x = 0.0, double y = 0.0, double z = 0.0)
+			: m_x(x), m_y(y), m_z(z)
+		{
+
+		}
+
+		void print()
+		{
+			std::cout << "Point(" << m_x << " , " << m_y << " , " << m_z << ")\n";
+		}
+
+		void moveByVector(const Vector3D& v)
+		{
+			m_x += v.m_x;
+			m_y += v.m_y;
+			m_z += v.m_z;
+		}
+	};
+
+
+	void print()
+	{
+		std::cout << "Vector(" << m_x << " , " << m_y << " , " << m_z << ")\n";
+	}
+};
+
+
+int main()
+{
+	Vector3D::Point3D p(3.0, 4.0, 5.0);
+	Vector3D v(3.0, 3.0, -2.0);
+
+	p.print();
+	v.print();
+	p.moveByVector(v);
+	p.print();
+
+	return 0;
+}
